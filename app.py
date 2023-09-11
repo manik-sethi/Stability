12e02a40d35a5e3a0c5b898376c0caacc6cbcf2f
Manik <pardan@Maniks-MacBook-Pro.local>
2023-09-11 14:49:50 -0700

    deposits working

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask, request, render_template, redirect, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask import jsonify, request
 import bcrypt
-from datetime import datetime  # Import datetime
-
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 app.secret_key = 'secret_key'
 
+# Define SQLAlchemy models
 class Deposit(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
@@ -24,25 +23,21 @@ class Transaction(db.Model):
     amount = db.Column(db.Float, nullable=False)
     date = db.Column(db.String(100), nullable=False)
 
-#take out later dude
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
-    #balance = db.Column(db.String(100))
-    
 
     def __init__(self, email, password, name):
         self.name = name
         self.email = email
-        #self.balance = balance
         self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-    
-    def check_password(self,password):
-        return bcrypt.checkpw(password.encode('utf-8'),self.password.encode('utf-8'))
 
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+
+# Create the data tables
 with app.app_context():
     db.create_all()
 
@@ -60,93 +55,109 @@ def overview():
 
 @app.route('/deposits_and_transactions')
 def deposits_and_transactions():
-    '''if request.method == 'POST':
-        balance = request.form['balance']
-        new_balance = User(balance=balance)
-        db.session.commit()'''
-    return render_template('deposits_and_transactions.html')
+    if 'email' in session:
+        user = User.query.filter_by(email=session['email']).first()
+
+        # Fetch deposits and transactions from the database
+        deposits = Deposit.query.filter_by(user_id=user.id).all()
+        transactions = Transaction.query.filter_by(user_id=user.id).all()
+
+        return render_template('deposits_and_transactions.html', user=user, deposits=deposits, transactions=transactions)
+
+    return redirect('/login')
 
 @app.route('/budgeting')
 def budgeting():
     return render_template('budgeting.html')
 
-
-
-@app.route('/register', methods=['GET','POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        # handle request
+        # Handle user registration
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
-        
 
-        new_user = User(name=name,email=email,password=password)
+        new_user = User(name=name, email=email, password=password)
         db.session.add(new_user)
         db.session.commit()
         return redirect('/login')
 
     return render_template('register.html')
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-    
 
         user = User.query.filter_by(email=email).first()
-        
+
         if user and user.check_password(password):
             session['email'] = user.email
             return redirect('/overview')
         else:
-            return render_template('login.html',error='Invalid user')
+            return render_template('login.html', error='Invalid user')
 
     return render_template('login.html')
-
 
 @app.route('/dashboard')
 def dashboard():
     if 'email' in session:
         user = User.query.filter_by(email=session['email']).first()
 
-        # Fetch deposits and transactions from the database (you need to define these models)
+        # Fetch deposits and transactions from the database
         deposits = Deposit.query.filter_by(user_id=user.id).all()
         transactions = Transaction.query.filter_by(user_id=user.id).all()
 
         return render_template('dashboard.html', user=user, deposits=deposits, transactions=transactions)
-    return redirect('/login')
 
+    return redirect('/login')
 
 @app.route('/logout')
-
 def logout():
-    session.pop('email',None)
+    session.pop('email', None)
     return redirect('/login')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/create_transaction', methods=['POST'])
+def create_transaction():
+    if 'email' in session:
+        user = User.query.filter_by(email=session['email']).first()
 
-@app.route('/deposits_and_transactions')
-def deposits_and_transactions():
-    return render_template('deposits_and_transactions.html')
+        # Handle creating a new transaction based on user input
+        description = request.form['description']
+        category = request.form['category']
+        amount = request.form['amount']
+        date = datetime.now()  # You can use the current date/time
 
-@app.route('/store_deposit', methods=['POST'])
-def store_deposit():
-    # Handle storing the deposit data in the database
-    # You need to implement this logic based on your database setup
+        new_transaction = Transaction(user_id=user.id, description=description, category=category, amount=amount, date=date)
+        db.session.add(new_transaction)
+        db.session.commit()
 
-    # Example code for receiving data from the client:
-    data = request.get_json()
-    amount = data.get('amount')
-    date = data.get('date')
+        return redirect('/dashboard')
 
-    # Perform database operations to store the deposit
+    return redirect('/login')
 
-    # Return a response indicating success or failure
-    response_data = {'message': 'Deposit stored successfully'}
-    return jsonify(response_data), 200
+@app.route('/create_deposit', methods=['POST'])
+def create_deposit():
+    if 'email' in session:
+        user = User.query.filter_by(email=session['email']).first()
+
+        # Handle creating a new deposit based on user input
+        amount = request.form['deposit']  # Update to 'deposit' field
+        date = datetime.now()
+
+        try:
+            new_deposit = Deposit(user_id=user.id, amount=amount, date=date)
+            db.session.add(new_deposit)
+            db.session.commit()
+            return redirect('/dashboard')
+        except Exception as e:
+            print(f"Error storing deposit: {e}")
+            db.session.rollback()  # Rollback the transaction in case of an error
+
+    return redirect('/login')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
